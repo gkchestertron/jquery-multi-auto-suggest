8152a3c69d94ac22fbe8797736e5f056b183ba68
Fellman <fellmanj@pet-fellmanj-m.ds.ad.adp.com>
2016-05-10 21:43:20 -0700

    init commit

---
+++ mas.js
@@ -0,0 +1,10 @@
+/**
+ * @param {array[object]} optionsArr - array of options objects
+ */
+$.fn.multiAutoSuggest = function (optionsArr) {
+    var self = this; // the element
+
+    function autoSuggest(el, options) {
+
+    }
+};
