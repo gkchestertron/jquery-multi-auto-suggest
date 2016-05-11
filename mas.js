86db730e3f7e1e82ed1589af76785cfb36a62b27
Fellman <fellmanj@pet-fellmanj-m.ds.ad.adp.com>
2016-05-10 21:53:36 -0700

    rename and start the thing

--- mas.js
+++ mas.js
@@ -1,10 +1,19 @@
 /**
  * @param {array[object]} optionsArr - array of options objects
+ * @description - must pass path to json file (or object) and template and width and height for the drop-downs
  */
 $.fn.multiAutoSuggest = function (optionsArr) {
     var self = this; // the element
 
-    function autoSuggest(el, options) {
+    if (!Array.isArray(optionsArr))
+        throw new Error('$.fn.multiAutoSuggest requires an array of options objects');
 
+    $.each(optionsArr, function (idx, options) {
+        options.offset = ???; // calculate offset from length of optionsArr
+        autoSuggest(self, options);
+    });
+
+    function autoSuggest($input, options) {
+        // make the thing, using the offset 
     }
 };
